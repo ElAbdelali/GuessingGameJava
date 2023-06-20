4f13d097f9c3527df8523bde84613d279fdbf767
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-06-20 10:43:35 -0500

    Update message

--- Game.java
+++ Game.java
@@ -1,6 +1,6 @@
 /** A number guessing game. */
 public class Game {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Welcome to the guessing game!");
   }
 }
